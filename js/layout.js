5700e91e896fd2d9397220b2dd8b6658378d5683
Everton Vinicius <84684742+EvertonViniciusav@users.noreply.github.com>
2025-05-23 19:08:27 -0400

    Commit

---
+++ js/layout.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    fetch("./header.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("header").innerHTML=data
+        })
+        .catch((error) => console.error("Erro ao carregar o header ", error))
+
+    fetch("./footer.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("footer").innerHTML=data
+        })
+        .catch((error) => console.error("Erro ao carregar o footer ", error))
+})
